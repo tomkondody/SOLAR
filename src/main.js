187b7cb044984eaba79f1c8a8f87667b2b5a2047
TOM <thommu72@gmail.com>
2025-07-23 14:57:28 +0530

    textures path correction

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
-
+import s from '../textures'
 // ----------------------------------------------------------------
 // SETUP: Scene, Camera, Renderer
 // ----------------------------------------------------------------
@@ -46,16 +46,16 @@ scene.add(sunlight);
 // ----------------------------------------------------------------
 const loader = new THREE.TextureLoader();
 const textures = {
-  sun: loader.load('./textures/sun.jpg'),
-  mercury: loader.load('./textures/mercury.jpg'),
-  venus: loader.load('./textures/venus.jpg'),
-  earth: loader.load('./textures/earth.jpg'),
-  mars: loader.load('./textures/mars.jpg'),
-  jupiter: loader.load('./textures/jupiter.jpg'),
-  saturn: loader.load('./textures/saturn.jpg'),
-  uranus: loader.load('./textures/uranus.jpg'),
-  neptune: loader.load('./textures/neptune.jpg'),
-  stars: loader.load('./textures/stars.jpg'),
+  sun: loader.load('../textures/sun.jpg'),
+  mercury: loader.load('../textures/mercury.jpg'),
+  venus: loader.load('../textures/venus.jpg'),
+  earth: loader.load('../textures/earth.jpg'),
+  mars: loader.load('../textures/mars.jpg'),
+  jupiter: loader.load('../textures/jupiter.jpg'),
+  saturn: loader.load('../textures/saturn.jpg'),
+  uranus: loader.load('../textures/uranus.jpg'),
+  neptune: loader.load('../textures/neptune.jpg'),
+  stars: loader.load('../textures/stars.jpg'),
 };
 
 // Enable maximum anisotropic filtering and set each texture's encoding to sRGB
